e95c1d0f6a1d1642d955cde8ab967309e31d7bce
Uzurch1k <galcenkovlad7@gmail.com>
2025-02-16 21:04:25 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import './parts/lockscroll.js';
 import './parts/mobmenu.js';
 import './parts/modal.js';
-// import './parts/accordion.js';
-// import './parts/banner.js';
-// import './parts/splidecust.js';
-// import './parts/splide.js';
-// import './parts/forms.js';
-// import './parts/select.js';
+import './parts/navigation.js';
+import './parts/accordion.js';
+import './parts/splidecust.js';
+import './parts/splide.js';
+import './parts/forms.js';
